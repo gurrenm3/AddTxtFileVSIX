0b70cb386a041a49aee54acdb160031dc5b412b2
Thomas Margraff <thomaspmar@protonmail.com>
2021-08-03 12:15:50 -0700

    Add project files.

---
+++ AddTxtFileVSIX/MyExtensionInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Controls;
+
+namespace AddNewItem_Template.Shared
+{
+    public class MyExtensionInfo
+    {
+        public static string itemName = "Txt File";
+
+        public static string fileExtension = ".txt";
+
+        public static string checkboxText = "";
+
+        public const string addItemWindowGUID = "79aead8e-630c-4f47-a8cc-7264274146fb";
+
+        public static string GenerateFileText(string sourceFolder, string solutionDir, string newFileName, ref CheckBox checkBox)
+        {
+            return "";
+        }
+    }
+}
+
